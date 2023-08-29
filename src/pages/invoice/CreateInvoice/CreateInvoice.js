dfc3d0203fe2c2a451fe4fd5c09587a5c173dbac
Menna Ali <menna.ali@ditech.ae>
2023-08-29 10:52:26 +0300

    edit in file upload

--- src/pages/invoice/CreateInvoice/CreateInvoice.js
+++ src/pages/invoice/CreateInvoice/CreateInvoice.js
@@ -1,44 +1,25 @@
+import { Field, Form, Formik } from 'formik'
+import AppButton from '../../../components/common/AppButton/AppButton.styles'
 import Logic from './logic'
+import { Grid } from '@mui/material'
+import FileUpload from '../../../components/common/FileUpload/FileUpload.styles'
+import { useState } from 'react'
+import * as Yup from 'yup'
+import { maxFilesSizes } from '../../../utils/constants/config'
+import { TextField } from 'formik-mui'
 
 const CreateInvoice = ({ className, pagePermissions }) => {
   console.log(pagePermissions)
   const { handleClick, state } = Logic()
-
-  // const handleErrorEnter = (msg, url, lineNo, columnNo, error) => {
-  //   let string = msg.toLowerCase()
-  //   let substring = 'script error'
-  //   console.log(string)
-  //   if (string.indexOf(substring) > -1) {
-  //     alert('Script Error: See Browser Console for Detail')
-  //   } else {
-  //     // let message = ['Message: ' + msg, 'URL: ' + url, 'Line: ' + lineNo, 'Column: ' + columnNo, 'Error object: ' + JSON.stringify(error)].join(' - ')
-  //     const messageObj = {
-  //       Message: msg,
-  //       URL: url,
-  //       Line: lineNo,
-  //       Column: columnNo,
-  //       ErrorObject: JSON.stringify(error),
-  //     }
-  //     const messObj = JSON.stringify(messageObj)
-  //     console.log(messObj)
-  //     // ourLogger.log('info',messObj);
-  //     // maybe call an axios post passing in the error
-  //     // axios.post('/clinet-log',{messObj} )
-  //     //     .then( res => {
-  //     //         res
-  //     //     })
-  //     console.log(messageObj)
-  //     return false
-  //     return messObj
-  //   }
-  // }
-  // window.onerror = handleErrorEnter
+  const [FileType, setFileType] = useState('')
+  const [file, setFile] = useState(null)
 
   return (
     <div className={className}>
       CreateInvoice
       {state.test}
       <button onClick={handleClick}>update</button>
+      <AppButton onClick={handleClick}>update</AppButton>
     </div>
   )
 }

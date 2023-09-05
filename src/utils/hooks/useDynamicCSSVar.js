7c6086434d30cce33e2bf524b43a9a1845071a54
p.labib <p.labib@ditech.ae>
2023-09-05 17:36:40 +0300

    fix error message in useDynamicCssVar hook

--- src/utils/hooks/useDynamicCSSVar.js
+++ src/utils/hooks/useDynamicCSSVar.js
@@ -5,12 +5,18 @@ export const useDynamicCSSVar = (cssVarName) => {
 
   useEffect(() => {
     const element = elementRef.current
-    const resizeObserver = new ResizeObserver((entries) => {
-      for (let entry of entries) {
-        document.documentElement.style.setProperty(cssVarName, `${entry.contentRect.height}px`)
-      }
-    })
-
+    let resizeObserver
+    if (!element.__resizeObserver) {
+      resizeObserver = new ResizeObserver((entries) => {
+        for (let entry of entries) {
+          document.documentElement.style.setProperty(cssVarName, `${entry.contentRect.height}px`)
+        }
+      })
+  
+      element.__resizeObserver = resizeObserver
+    } else {
+      resizeObserver = element.__resizeObserver
+    }
     if (element) {
       resizeObserver.observe(element)
     }
